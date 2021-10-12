f562b8edf00ff21821439f98ef2f1fe2ae46db14
AckerTech <jamie.acker@mtbethelchristian.org>
2021-10-12 10:44:17 -0400

    Drew First Iteration

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Base/Controls/TeleOp/HelloWorld.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Base/Controls/TeleOp/HelloWorld.java
@@ -4,17 +4,19 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-//@Disabled
+@Disabled
 @TeleOp(name = "TeleOp:HelloWorld", group ="Lessons")
 
 public class HelloWorld extends OpMode {
     @Override
     public void init() {
+
         telemetry.addData("Hello", "World");
     }
     @Override
     public void loop() {
 
+        telemetry.addData("I love", " Robots");
     }
 }
 // This is a single line comment
