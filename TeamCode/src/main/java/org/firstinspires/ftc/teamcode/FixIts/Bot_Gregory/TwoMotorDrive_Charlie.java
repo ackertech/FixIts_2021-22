8bab0a829adae40489993da21aa9591b90fcecd6
charlie.kopkas <Charlie.Kopkas@mtbethelchristian.org>
2021-10-26 14:59:08 -0400

    First code V3

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FixIts/Bot_Gregory/TwoMotorDrive_Charlie.java
@@ -0,0 +1,68 @@
+package org.firstinspires.ftc.teamcode.FixIts.Bot_Gregory;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.checkerframework.checker.units.qual.A;
+
+public class TwoMotorDrive_Charlie {
+
+    //Declaring variables for my motor
+    public DcMotor frontLeftMotor;
+    public DcMotor frontRightMotor;
+
+    // Reguired to run robot
+    public LinearOpMode linearOp = null;
+    public void setLinearOp(LinearOpMode linearOp) { this.linearOp = linearOp; }
+
+    //Common Method for Motor Run Mode
+    public final DcMotor.RunMode currentMotorRunMode = DcMotor.RunMode.RUN_USING_ENCODER;
+    public static final double TICKS_PER_ROTATION = 538;
+
+    public void setMotorRunModes (DcMotor.RunMode mode) {
+
+        frontLeftMotor.setMode(mode);
+        frontRightMotor.setMode(mode);
+
+    }
+
+    public void stopMotors () {
+        frontLeftMotor.setPower(0);
+        frontRightMotor.setPower(0);
+
+    }
+
+    public void driveForward (double power) {
+
+        double ABSpower = Math.abs(power);
+        frontLeftMotor.setPower(ABSpower);
+
+    }
+
+    public void driveBackward (double power) {
+
+        double ABSpower = Math.abs(power);
+        frontLeftMotor.setPower(-ABSpower);
+        frontRightMotor.setPower(-ABSpower);
+// hi
+    }
+
+    public void rotateLeft (double power) {
+
+        double ABSpower = Math.abs(power);
+        frontLeftMotor.setPower(-ABSpower);
+        frontRightMotor.setPower(-ABSpower);
+
+    }
+
+    public void rotateRight (double power) {
+        double ABSpower = Math.abs(power);
+
+        frontLeftMotor.setPower(ABSpower);
+        frontRightMotor.setPower(-ABSpower);
+        
+    }
+
+}
+
+// push jda2
