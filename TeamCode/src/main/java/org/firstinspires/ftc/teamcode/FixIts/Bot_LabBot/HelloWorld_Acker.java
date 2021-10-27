9215718eb4b46bd0743c21daba11b05a9f47f755
AckerTech <jamie.acker@mtbethelchristian.org>
2021-10-27 13:10:55 -0400

    Fix-Its Project Renaming

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FixIts/Bot_LabBot/HelloWorld_Acker.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FixIts/Bot_LabBot/HelloWorld_Acker.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-//@Disabled
+@Disabled
 @TeleOp(name="Hello World Acker")
 
 public class HelloWorld_Acker extends OpMode {
