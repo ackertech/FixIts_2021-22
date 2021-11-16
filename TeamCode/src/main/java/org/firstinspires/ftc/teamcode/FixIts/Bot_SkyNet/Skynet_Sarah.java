d43fae530f4026c8ad10a29d3119bedbe757891a
sarah.decker <sarah.decker@mtbethelchristian.org>
2021-11-16 14:24:06 -0500

    V1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FixIts/Bot_SkyNet/Skynet_Sarah.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FixIts/Bot_SkyNet/Skynet_Sarah.java
@@ -46,18 +46,18 @@ public class Skynet_Sarah extends Twomotordrive_Sarah{
     }
 
     public void initFlag() {
-        flag.setPosition(0.1);
+        flag.setPosition(0);
     }
     public void raiseFlag() {
-        flag.setPosition(0.9);
+        flag.setPosition(90);
     }
     public void lowerFlag() {
-        flag.setPosition(0.1);
+        flag.setPosition(90);
     }
     public void waveFlagRight() {
-        flag.setPosition(0.1);
+        flag.setPosition(25);
     }
     public void waveFlagLeft() {
-        flag.setPosition(0.1);
+        flag.setPosition(25);
     }
 }
