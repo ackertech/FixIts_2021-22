f23535c6d670ec7e710db122ec0d4a04f8d5a6b4
AckerTech <jamie.acker@mtbethelchristian.org>
2022-02-10 16:15:50 -0500

    Resolving AckerBotCode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FixIts/Bot_LabBot/AutoLabot_4Motor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FixIts/Bot_LabBot/AutoLabot_4Motor.java
@@ -25,9 +25,76 @@ public class AutoLabot_4Motor extends LinearOpMode {
 
         while (opModeIsActive()){
 
+            telemetry.addLine("Driving Forward from Start");
+            telemetry.update();
+            Bot.driveForward(1);
+            sleep(2000);
+            Bot.stopMotors();
 
+            telemetry.addLine("Driving Forward in Square");
+            telemetry.update();
+            Bot.driveForward(1);
+            sleep(2000);
+            Bot.stopMotors();
+
+            //We are now at the top left corner of the square
+
+            telemetry.addLine("Turning Right at First Corner");
+            telemetry.update();
+            Bot.rotateRight(0.5);
+            sleep(1000);
+            Bot.stopMotors();
+
+            telemetry.addLine("Driving Forward to Second Corner");
+            telemetry.update();
+            Bot.driveForward(1);
+            sleep(2000);
+            Bot.stopMotors();
+
+            //We are at the top right corner of the square
+
+            telemetry.addLine("Turning Right at Second Corner");
+            telemetry.update();
+            Bot.rotateRight(0.5);
+            sleep(1000);
+            Bot.stopMotors();
+
+            telemetry.addLine("Driving Forward to Third Corner");
+            telemetry.update();
+            Bot.driveForward(1);
+            sleep(2000);
+            Bot.stopMotors();
+
+            //We are at the bottom right corner of the square
+
+            telemetry.addLine("Turning Right at Third Corner");
+            telemetry.update();
+            Bot.rotateRight(0.5);
+            sleep(1000);
+            Bot.stopMotors();
+
+            telemetry.addLine("Driving Forward to Fourth Corner ");
+            telemetry.update();
+            Bot.driveForward(1);
+            sleep(2000);
+            Bot.stopMotors();
+
+            //We are at the bottom left corner of the square
+
+            telemetry.addLine("Turning Left at Fourth Corner");
+            telemetry.update();
+            Bot.rotateLeft(-0.5);
+            sleep(1000);
+            Bot.stopMotors();
+
+            telemetry.addLine("Driving Forward to Finish");
+            telemetry.update();
+            Bot.driveForward(1);
+            sleep(2000);
+            Bot.stopMotors();
 
             requestOpModeStop();
+
         }
 
         idle();
