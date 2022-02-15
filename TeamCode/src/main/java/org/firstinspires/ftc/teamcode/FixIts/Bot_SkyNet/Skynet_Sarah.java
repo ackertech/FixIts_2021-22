863c5065648b7ee4855e50b5c664f4facedaefec
PLAYER12345678910 <jack.stefan@mtbethelchristian.org>
2022-02-15 14:12:14 -0500

    Here is my code.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FixIts/Bot_SkyNet/Skynet_Sarah.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FixIts/Bot_SkyNet/Skynet_Sarah.java
@@ -74,11 +74,10 @@ public class Skynet_Sarah extends Twomotordrive_Sarah{
     public void waveFlagLeft() {
         flag.setPosition(0.35);
     }
+        public void setLedPattern (RevBlinkinLedDriver.BlinkinPattern patternName) {
 
-//    public void setLedPattern (RevBlinkinLedDriver.BlinkinPattern patternName) {
-
- //       ledLights.setPattern(patternName);
- //   }
+       ledLights.setPattern(patternName);
+    }
 
 
 }
