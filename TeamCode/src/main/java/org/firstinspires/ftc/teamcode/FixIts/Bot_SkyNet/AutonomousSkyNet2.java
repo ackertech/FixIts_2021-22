62b3c36ffd3c7928eb56b7f8e6d35ea94908c3f8
sarah.decker <sarah.decker@mtbethelchristian.org>
2022-02-15 14:48:59 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FixIts/Bot_SkyNet/Skynet_Sarah.java
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FixIts/Bot_SkyNet/TeleOpSkyNet_Sarah.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FixIts/Bot_SkyNet/AutonomousSkyNet2.java
@@ -0,0 +1,63 @@
+package org.firstinspires.ftc.teamcode.FixIts.Bot_SkyNet;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+
+
+
+@Autonomous(name = "Skynet Auto 2")
+
+public class AutonomousSkyNet2 extends LinearOpMode {
+
+    //Specify the robot class to construct
+    Skynet_Sarah Bot = new Skynet_Sarah();
+
+    //We are adding in the code that is personal to the robot here
+    @Override
+
+    public void runOpMode() throws InterruptedException {
+
+        Bot.initRobot(hardwareMap);
+
+
+        //waiting for drive to press start on drive control system
+        waitForStart();
+
+
+        while (opModeIsActive()) {
+
+            telemetry.addLine("Driving Forward in Square");
+            telemetry.update();
+            Bot.driveForward(1, 1);
+
+            telemetry.addLine("Turning Right at First Corner");
+            telemetry.update();
+            Bot.turnRight(1, 1);
+
+            telemetry.addLine("Driving Forward in Square");
+            telemetry.update();
+            Bot.driveForward(1, 1);
+
+            telemetry.addLine("Turning Right at Second Corner");
+            telemetry.update();
+            Bot.turnRight(1, 1);
+
+            telemetry.addLine("Driving Forward in Square");
+            telemetry.update();
+            Bot.driveForward(1, 1);
+
+            telemetry.addLine("Turning Left at Third Corner");
+            telemetry.update();
+            Bot.turnLeft(1, 1);
+
+            telemetry.addLine("Driving Forward in Square");
+            telemetry.update();
+            Bot.driveForward(1, 1);
+
+            Bot.stopMotors();
+
+
+        }
+    }
+}
